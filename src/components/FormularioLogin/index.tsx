95fffbd19026cb248365c7b0d9dcba32ef252fc1
ManoelGomes <manoel_gomes@outlook.com>
2021-11-19 21:30:23 -0400

    rota

--- src/components/FormularioLogin/index.tsx
+++ src/components/FormularioLogin/index.tsx
@@ -1,4 +1,4 @@
-import React,{/*FormEvent,*/ useState}from 'react';
+import React/*,{FormEvent, useState}*/from 'react';
 //import {Formik} from 'formik';
 import { NavLink, useHistory} from 'react-router-dom';
 //import { TextField } from '../TextField';
